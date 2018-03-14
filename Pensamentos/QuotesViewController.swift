04e07cd1a122d4130976efb6c07921fad3a66083
Usuário Convidado <Guest@pa06micro31.local>
2018-03-14 20:35:06 -0300

    Primeira hora

--- Pensamentos/QuotesViewController.swift
+++ Pensamentos/QuotesViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class QuotesViewController: UIViewController {
 
+    
+    @IBOutlet weak var ivPhoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
