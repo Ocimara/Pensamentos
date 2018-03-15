391bd08900d04243d3b1b2ec65061401e44f325d
Usuário Convidado <Guest@pa06micro31.local>
2018-03-14 22:09:50 -0300

    Após intervalo

--- Pensamentos/QuotesViewController.swift
+++ Pensamentos/QuotesViewController.swift
@@ -12,16 +12,61 @@ class QuotesViewController: UIViewController {
 
     
     @IBOutlet weak var ivPhoto: UIImageView!
+    @IBOutlet weak var lbAutor: UILabel!
+    @IBOutlet weak var lbQuotes: UILabel!
+    
+    let config = Configuration.shared
+    
+    let quotesManager = QuotesManager.shared
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        formatView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        prepareQuote()
+    }
+    
+    func formatView() {
+        view.backgroundColor = config.colorScheme == 0 ? .white : UIColor(red: 156.0/255.0, green: 68.0/255.0, blue: 15.0/255.0, alpha: 1.0)
+        lbQuotes.textColor = config.colorScheme == 0 ? .black : .white
+        lbAutor.textColor = config.colorScheme == 0 ? UIColor(red: 192.0/255.0, green: 96.0/255.0, blue: 49.0/255.0, alpha: 1.0) : .yellow
+        prepareQuote()
+    }
+    
+    func prepareQuote() {
+        timer?.invalidate()
+        if config.autorefresh {
+            timer = Timer.scheduledTimer(withTimeInterval: config.timeInterval, repeats: true) { (timer) in
+                self.showRandomQuote()
+            }
+        }
+        showRandomQuote()
+    }
+    
+    func showRandomQuote() {
+        let quote = quotesManager.getRandomQuote()
+        lbQuotes.text = quote.quote
+        lbAutor.text = quote.author
+        ivPhoto.image = UIImage(named: quote.image)
+        
+        lbQuotes.alpha = 0.0
+        lbAutor.alpha = 0.0
+        ivPhoto.alpha = 0.0
+        
+        UIView.animate(withDuration: 2.5) {
+            self.lbQuotes.alpha = 1.0
+            self.lbAutor.alpha = 1.0
+            self.ivPhoto.alpha = 1.0
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
